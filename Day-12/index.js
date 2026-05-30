445c88b624b563b49b0e9d077235fcfb593adaa4
rishav-026 <rishavkumar7034@gmail.com>
2026-05-30 18:08:56 +0530

    Completed day-12

---
+++ Day-12/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const users = require('./MOCK_DATA.json')
+const fs = require('fs');
+
+const app = express();
+// app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+//Handler function
+app.use((req,res,next)=>{
+    fs.appendFile('logs.txt',`\n${Date.now()}:${req.method}:${req.path}\n`,(err,data)=>{
+        if(err) console.error('Error writing to log file');
+    })
+    next();
+})
+
+const PORT = 8000;
+
+//Routes
+app.get('/',(req,res)=>{
+    return res.send("Hello express");
+})
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+app.get('/users',(req,res)=>{
+    const html = `
+    <ul>
+        ${users.map(user => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
+    </ul>
+    `
+    return res.send(html);
+})
+app.get('/api/users/:id',)
+
+
+app.route('/api/users/:id').get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find(user=>user.id===id)
+    return res.json(user);
+})
+.patch((req,res)=>{
+    return res.json({status:"pending"})
+})
+.delete((req,res)=>{
+    const id = Number(req.params.id);
+    const index = users.findIndex(user=>user.id===id);
+
+    if(index===-1) return res.json({status:"failed",message:"User not found"})
+    users.splice(index,1);
+    res.json({status:"success",message:"User deleted successfully"})
+});
+
+
+
+app.post('/api/users',(req,res)=>{
+   const body = req.body;
+   users.push({...body,id:users.length+1});
+   fs.writeFileSync('./MOCK_dATA.json', JSON.stringify(users),(err,data)=>{
+    if(err) return res.json({status:"failed"})
+    return res.json({status:"success" , id:users.length})
+   });
+    
+});
+app.patch('/api/users/:id',(req,res)=>{
+    return res.json({status:"success"})
+})
+
+app.delete('/api/users/:id',(req,res)=>{
+
+    return res.json({status:"success"})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
